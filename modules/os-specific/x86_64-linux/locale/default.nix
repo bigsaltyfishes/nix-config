202b87242e676edd619e3d8396cd2b02adaaf56e
bigsaltyfishes <bigsaltyfishes@gmail.com>
2024-10-30 20:21:50 +0800

    locale: Follow nixpkgs changes

--- modules/os-specific/x86_64-linux/locale/default.nix
+++ modules/os-specific/x86_64-linux/locale/default.nix
@@ -7,7 +7,8 @@
     };
     supportedLocales = [ "en_US.UTF-8/UTF-8" "zh_CN.UTF-8/UTF-8" ];
     inputMethod = {
-      enabled = "ibus";
+      enable = true;
+      type = "ibus";
       ibus.engines = with pkgs.ibus-engines; [ libpinyin ];
     };
   };
