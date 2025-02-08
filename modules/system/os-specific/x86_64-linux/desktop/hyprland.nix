82200160fa88972b6307510b3e7518ecb05b9aaa
bigsaltyfishes <bigsaltyfishes@gmail.com>
2025-02-08 20:21:07 +0800

    hyprland: enable udisks2
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- modules/system/os-specific/x86_64-linux/desktop/hyprland.nix
+++ modules/system/os-specific/x86_64-linux/desktop/hyprland.nix
@@ -26,6 +26,7 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.dconf.enable = true;
+    services.udisks2.enable = true;
     services.gnome.gnome-keyring.enable = true;
     programs.hyprland = {
       enable = true;
