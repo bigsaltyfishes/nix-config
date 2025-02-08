4ffe360e378afb09a2a4a2cc5b2568623124dea7
bigsaltyfishes <bigsaltyfishes@gmail.com>
2025-02-08 20:41:16 +0800

    programs: clash-verge: open firewall
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- modules/system/os-specific/x86_64-linux/programs/clash-verge/default.nix
+++ modules/system/os-specific/x86_64-linux/programs/clash-verge/default.nix
@@ -5,5 +5,9 @@ in
 {
   config = lib.mkIf (lib.elem "extraApps" profile.extraFeatures || lib.elem "full" profile.extraFeatures) {
     programs.clash-verge.enable = true;
+    networking.firewall = {
+      allowedTCPPorts = [ 7897 ];
+      allowedUDPPorts = [ 7897 ];
+    };
   };
 }
